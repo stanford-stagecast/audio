a335181c7245227c0e0db20c22a507b0807cf4fa
Jerry Chen <jchen18@sophon.stanford.edu>
2020-10-07 21:38:41 -0700

    track packets received

--- internetStatistics/src/client.cc
+++ internetStatistics/src/client.cc
@@ -11,39 +11,41 @@
 using namespace std;
 
 const uint64_t MAX_NUM_PACKETS = 100000;
-const uint64_t DELAY { 1'000'000 };
+const uint64_t DELAY{ 1'000'000 };
 // Address server {"171.67.76.46", 9090};
-Address server { "127.0.0.1", 9090 };
+Address server{ "127.0.0.1", 9090 };
 
-void split_on_char( const string_view str, const char ch_to_find, vector<string_view>& ret )
+void split_on_char(const string_view str, const char ch_to_find, vector<string_view>& ret)
 {
   ret.clear();
   bool in_double_quoted_string = false;
   unsigned int field_start = 0;
-  for ( unsigned int i = 0; i < str.size(); i++ ) {
+  for (unsigned int i = 0; i < str.size(); i++) {
     const char ch = str[i];
-    if ( ch == '"' ) {
+    if (ch == '"') {
       in_double_quoted_string = !in_double_quoted_string;
-    } else if ( in_double_quoted_string ) {
+    }
+    else if (in_double_quoted_string) {
       continue;
-    } else if ( ch == ch_to_find ) {
-      ret.emplace_back( str.substr( field_start, i - field_start ) );
+    }
+    else if (ch == ch_to_find) {
+      ret.emplace_back(str.substr(field_start, i - field_start));
       field_start = i + 1;
     }
   }
-  ret.emplace_back( str.substr( field_start ) );
+  ret.emplace_back(str.substr(field_start));
 }
 
-string build_packet( int packet_counter )
+string build_packet(int packet_counter)
 {
-  return std::to_string( packet_counter );
+  return std::to_string(packet_counter);
 }
 
 void program_body()
 {
   EventLoop event_loop;
   UDPSocket client_sock;
-  client_sock.set_blocking( false );
+  client_sock.set_blocking(false);
   uint64_t start_time = Timer::timestamp_ns();
   uint64_t prev_time = start_time;
   uint64_t current_time = start_time;
@@ -53,18 +55,18 @@ void program_body()
     client_sock,
     Direction::Out,
     [&] {
-      if ( prev_time + DELAY < current_time ) {
+      if (prev_time + DELAY < current_time) {
         cout << "Packet counter: " << packet_counter << endl;
-        string packet_content = build_packet( packet_counter );
-        client_sock.sendto( server, packet_content );
+        string packet_content = build_packet(packet_counter);
+        client_sock.sendto(server, packet_content);
         packet_counter++;
         prev_time = current_time;
       }
       current_time = Timer::timestamp_ns();
     },
-    [&] { return packet_counter < MAX_NUM_PACKETS; } );
+    [&] { return packet_counter < MAX_NUM_PACKETS; });
 
-  while ( event_loop.wait_next_event( 5 ) != EventLoop::Result::Exit ) {
+  while (event_loop.wait_next_event(5) != EventLoop::Result::Exit) {
     /*if (Timer::timestamp_ns() - start_time > 5ULL * 1000 * 1000 * 1000) {
       cout << " timeout\n";
       return;
@@ -72,13 +74,14 @@ void program_body()
   }
 }
 
-int main( const int argc, const char* const argv[] )
+int main(const int argc, const char* const argv[])
 {
   try {
     global_timer();
     program_body();
     cout << global_timer().summary() << "\n";
-  } catch ( const exception& e ) {
+  }
+  catch (const exception& e) {
     cout << "Exception: " << e.what() << endl;
     cout << global_timer().summary() << "\n";
     return EXIT_FAILURE;
--- internetStatistics/src/server.cc
+++ internetStatistics/src/server.cc
@@ -11,51 +11,61 @@
 #include "socket.hh"
 #include "timer.hh"
 
-const uint64_t DELAY { 1'000'000 };
+const uint64_t DELAY{ 1'000'000 };
 
 using namespace std;
 
-void split_on_char( const string_view str, const char ch_to_find, vector<string_view>& ret )
+void split_on_char(const string_view str, const char ch_to_find, vector<string_view>& ret)
 {
   ret.clear();
   bool in_double_quoted_string = false;
   unsigned int field_start = 0;
-  for ( unsigned int i = 0; i < str.size(); i++ ) {
+  for (unsigned int i = 0; i < str.size(); i++) {
     const char ch = str[i];
-    if ( ch == '"' ) {
+    if (ch == '"') {
       in_double_quoted_string = !in_double_quoted_string;
-    } else if ( in_double_quoted_string ) {
+    }
+    else if (in_double_quoted_string) {
       continue;
-    } else if ( ch == ch_to_find ) {
-      ret.emplace_back( str.substr( field_start, i - field_start ) );
+    }
+    else if (ch == ch_to_find) {
+      ret.emplace_back(str.substr(field_start, i - field_start));
       field_start = i + 1;
     }
   }
-  ret.emplace_back( str.substr( field_start ) );
+  ret.emplace_back(str.substr(field_start));
 }
 
-void export_data( vector<int64_t>& buffer_vals )
+void export_data(vector<int64_t>& buffer_vals, vector<int>& packets_received)
 {
   std::fstream fout;
-  fout.open( "buffer_every_one_ms.csv", std::ios::out | std::ios::app );
-  for ( size_t i = 0; i < buffer_vals.size(); i++ ) {
-    fout << buffer_vals.at( i ) << "\n";
+  fout.open("buffer_every_one_ms.csv", std::ios::out);
+  for (size_t i = 0; i < buffer_vals.size(); i++) {
+    fout << buffer_vals.at(i) << "\n";
   }
+  fout.close();
+
+  fout.open("packets_received.csv", std::ios::out);
+  for (size_t i = 0; i < packets_received.size(); i++) {
+    fout << packets_received.at(i) << "\n";
+  }
+  fout.close();
 }
 
-void program_body( vector<int64_t>& buffer_vals )
+void program_body(vector<int64_t>& buffer_vals, vector<int>& packets_received)
 {
   EventLoop event_loop;
   UDPSocket server_sock;
-  server_sock.set_blocking( true );
-  server_sock.bind( { "0", 9090 } );
+  server_sock.set_blocking(true);
+  server_sock.bind({ "0", 9090 });
 
   uint64_t start_time = Timer::timestamp_ns();
   uint64_t prev_time = start_time;
   uint64_t current_time = start_time;
-  uint64_t server_packet_counter = 1;
+  atomic<uint64_t> server_packet_counter = 1;
   atomic<int64_t> buffer = 0;
   std::mutex mtx_buf_vec;
+  std::mutex mtx_packet_vec;
   bool received = false;
 
   event_loop.add_rule(
@@ -64,32 +74,40 @@ void program_body( vector<int64_t>& buffer_vals )
     Direction::In,
     [&] {
       received = true;
-      if ( prev_time + DELAY < current_time ) {
+      if (prev_time + DELAY < current_time) {
         buffer--;
         auto recv = server_sock.recv();
         string payload = recv.payload;
-        uint64_t packet_number = stoull( payload );
+        uint64_t packet_number = stoull(payload);
 
-        if ( packet_number == server_packet_counter ) {
+        if (packet_number == server_packet_counter) {
           buffer++;
           server_packet_counter++;
-        } else if ( packet_number > server_packet_counter ) {
+
+          unique_lock<mutex> lk(mtx_packet_vec);
+          packets_received.push_back(1);
+        }
+        else if (packet_number > server_packet_counter) {
           cout << "skipping packets " << server_packet_counter << " to " << packet_number - 1 << endl;
-          buffer += ( server_packet_counter - packet_number ) + 1;
+          buffer += (server_packet_counter - packet_number) + 1;
+          unique_lock<mutex> lk(mtx_packet_vec);
+          for (size_t i = server_packet_counter; i < packet_number; ++i) {
+            packets_received.push_back(0);
+          }
+          packets_received.push_back(1);
           server_packet_counter = packet_number + 1;
         }
         prev_time = current_time;
-        mtx_buf_vec.lock();
-        buffer_vals.push_back( buffer );
-        mtx_buf_vec.unlock();
+        unique_lock<mutex> lk(mtx_buf_vec);
+        buffer_vals.push_back(buffer);
         cout << "received packet " << packet_number << " current buffer " << buffer << endl;
       }
       current_time = Timer::timestamp_ns();
     },
-    [&] { return server_packet_counter < 100000; } );
+    [&] { return server_packet_counter < 100000; });
 
-  while ( event_loop.wait_next_event( 5 ) != EventLoop::Result::Exit ) {
-    if ( received && prev_time + DELAY < current_time ) { // TODO: Determine whether this section is necessary
+  while (event_loop.wait_next_event(5) != EventLoop::Result::Exit) {
+    if (received && prev_time + DELAY < current_time) { // TODO: Determine whether this section is necessary
       // buffer--; // race condition? not sure how the event handler is implemented
       // cout << "buffer: " << buffer << endl;
       // prev_time = current_time;
@@ -103,10 +121,12 @@ int main()
   try {
     global_timer();
     vector<int64_t> buffer_values;
-    program_body( buffer_values );
-    export_data( buffer_values );
+    vector<int> packets_received;
+    program_body(buffer_values, packets_received);
+    export_data(buffer_values, packets_received);
     cout << global_timer().summary() << "\n";
-  } catch ( const exception& e ) {
+  }
+  catch (const exception& e) {
     cout << "Exception: " << e.what() << endl;
     cout << global_timer().summary() << "\n";
     return EXIT_FAILURE;
