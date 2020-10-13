158d6d9d5a8c15ac160446e1c902e66eac89bdc1
Keith Winstein <keithw@cs.stanford.edu>
2020-10-12 18:02:51 -0700

    Make client send at steady pace

--- src/stats/client.cc
+++ src/stats/client.cc
@@ -2,35 +2,45 @@
 #include "exception.hh"
 #include "socket.hh"
 #include "timer.hh"
+#include <chrono>
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
 #include <optional>
+#include <thread>
 #include <unistd.h>
 
 using namespace std;
+using namespace std::chrono;
 
 const uint64_t MAX_NUM_PACKETS = 100;
-const uint64_t DELAY{ 1'000'000 };
-Address server {"171.67.76.94", 9090};
+const uint64_t DELAY { 1'000'000 };
+Address server { "171.67.76.94", 9090 };
 
-string build_packet(int packet_counter)
+string build_packet( int packet_counter )
 {
-  return std::to_string(packet_counter) + "12345678"; //40 byte packet
+  return std::to_string( packet_counter ) + "12345678"; // 40 byte packet
 }
 
 void program_body()
 {
-  EventLoop event_loop;
   UDPSocket client_sock;
-  client_sock.set_blocking(false);
-  uint64_t packet_counter = 1;
-  while (packet_counter < MAX_NUM_PACKETS) {
-    usleep(2500);
+  client_sock.set_blocking( false );
+
+  const auto transmission_interval = microseconds( 2500 );
+
+  auto time_for_next_transmission = steady_clock::now() + transmission_interval;
+
+  for ( unsigned int packet_counter = 0; packet_counter < MAX_NUM_PACKETS; packet_counter++ ) {
+    auto duration_to_sleep = time_for_next_transmission - steady_clock::now();
+    if ( duration_to_sleep.count() > 0 ) {
+      this_thread::sleep_for( duration_to_sleep );
+    }
+    time_for_next_transmission += transmission_interval;
+
     cout << "Packet counter: " << packet_counter << endl;
-    string packet_content = build_packet(packet_counter);
-    client_sock.sendto(server, packet_content);
-    packet_counter++;
+    string packet_content = build_packet( packet_counter );
+    client_sock.sendto( server, packet_content );
   }
 }
 
@@ -40,8 +50,7 @@ int main()
     global_timer();
     program_body();
     cout << global_timer().summary() << "\n";
-  }
-  catch (const exception& e) {
+  } catch ( const exception& e ) {
     cout << "Exception: " << e.what() << endl;
     cout << global_timer().summary() << "\n";
     return EXIT_FAILURE;
