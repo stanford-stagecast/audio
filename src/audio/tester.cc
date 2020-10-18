a1eea1e44cf153dca8fba04fb1cc949c47b07754
Keith Winstein <keithw@cs.stanford.edu>
2020-10-18 14:41:06 -0700

    tester.cc: search for device

--- src/audio/tester.cc
+++ src/audio/tester.cc
@@ -4,16 +4,48 @@
 
 using namespace std;
 
-int main()
+pair<string, string> find_device( const string_view expected_description )
 {
   ALSADevices devices;
-  auto device_list = devices.list();
+  bool found = false;
 
-  for ( const auto& dev : device_list ) {
-    cout << dev.name << " has these outputs:\n";
+  string name, output_name;
+
+  for ( const auto& dev : devices.list() ) {
     for ( const auto& output : dev.outputs ) {
-      cout << "     " << output.first << " = " << output.second << "\n";
+      if ( output.second == expected_description ) {
+        if ( found ) {
+          throw runtime_error( "Multiple devices matching description" );
+        } else {
+          found = true;
+          name = dev.name;
+          output_name = output.first;
+        }
+      }
     }
+  }
+
+  if ( not found ) {
+    throw runtime_error( "device \"" + string( expected_description ) + "\" not found" );
+  }
+
+  return { name, output_name };
+}
+
+void program_body()
+{
+  const auto [name, output_name] = find_device( "UAC-2, USB Audio" );
+
+  cout << "Device found, nice name is " << name << " and output name is " << output_name << "\n";
+}
+
+int main()
+{
+  try {
+    program_body();
+  } catch ( const exception& e ) {
+    cout << e.what() << "\n";
+    return EXIT_FAILURE;
   }
 
   return EXIT_SUCCESS;
