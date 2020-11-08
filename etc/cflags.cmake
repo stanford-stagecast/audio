bf9ec8cfb2e86f30ec9ed99e785b0660ba53df03
Keith Winstein <keithw@cs.stanford.edu>
2020-11-08 14:21:08 -0800

    Cleanups

--- etc/cflags.cmake
+++ etc/cflags.cmake
@@ -1,9 +1,7 @@
 set (CMAKE_CXX_STANDARD 17)
 set (CMAKE_EXPORT_COMPILE_COMMANDS ON)
 set (CMAKE_BASE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
-set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17 -g -pedantic -pedantic-errors -Werror -Wall -Wextra -Wshadow -Wpointer-arith -Wcast-qual -Wformat=2 -Weffc++")
-
-#set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17")
+set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17 -g -pedantic -pedantic-errors -Werror -Wall -Wextra -Wshadow -Wpointer-arith -Wcast-qual -Wformat=2 -Weffc++ -march=native -mtune=native")
 
 # check for supported compiler versions
 set (IS_GNU_COMPILER ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU"))
--- src/README.md
+++
@@ -1,4 +0,0 @@
-# Internet Statistics
-
-## Build Directories
-Run `make` in the `/src` directory
