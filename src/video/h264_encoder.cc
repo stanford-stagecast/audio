56aaa3fa845fbc68fd879c4ca46dff9246f1bb49
Richard Lin <yifengl@stanford.edu>
2020-11-12 15:41:58 -0800

    Add H264Encoder class

---
+++ src/video/h264_encoder.cc
@@ -0,0 +1,67 @@
+#include "h264_encoder.hh"
+#include "exception.hh"
+
+using namespace std;
+
+H264Encoder::H264Encoder( const uint16_t width,
+                          const uint16_t height,
+                          const uint8_t fps,
+                          const string& preset,
+                          const string& tune )
+  : width_( width )
+  , height_( height )
+  , fps_( fps )
+{
+  if ( x264_param_default_preset( &params_, preset.c_str(), tune.c_str() ) != 0 ) {
+    throw runtime_error( "Error: Failed to set preset on x264." );
+  }
+  // Set params for encoder
+  params_.i_threads = 6;
+  params_.i_width = width_;
+  params_.i_height = height_;
+  params_.i_fps_num = fps_;
+  params_.i_fps_den = 1;
+  params_.b_annexb = 1;
+  params_.b_repeat_headers = 1;
+  params_.i_keyint_max = fps_ * 2;
+  params_.b_intra_refresh = true;
+
+  // Apply profile
+  if ( x264_param_apply_profile( &params_, "high" ) != 0 ) {
+    throw runtime_error( "Error: Failed to set baseline profile on x264." );
+  }
+  encoder_.reset( notnull( "x264_encoder_open", x264_encoder_open( &params_ ) ) );
+
+  x264_picture_init( &pic_in_ );
+}
+
+string_view H264Encoder::encode( RasterYUV420& raster )
+{
+  if ( raster.width() != width_ or raster.height() != height_ ) {
+    throw runtime_error( "H264Encoder::encode(): size mismatch" );
+  }
+
+  pic_in_.img.i_csp = X264_CSP_I420;
+  pic_in_.img.i_plane = 3;
+  pic_in_.img.i_stride[0] = raster.width();
+  pic_in_.img.i_stride[1] = raster.chroma_width();
+  pic_in_.img.i_stride[2] = raster.chroma_width();
+
+  pic_in_.img.plane[0] = raster.Y_row( 0 );
+  pic_in_.img.plane[1] = raster.Cb_row( 0 );
+  pic_in_.img.plane[2] = raster.Cr_row( 0 );
+
+  pic_in_.i_pts = 90000 * frame_num_ / fps_;
+  frame_num_++;
+
+  int nals_count = 0;
+  x264_nal_t* nal;
+  x264_picture_t pic_out;
+  x264_encoder_encode( encoder_.get(), &nal, &nals_count, &pic_in_, &pic_out );
+
+  if ( not nal or nal->i_payload <= 0 ) {
+    return {};
+  }
+
+  return { reinterpret_cast<const char*>( nal->p_payload ), size_t( nal->i_payload ) };
+}
---
+++ src/video/h264_encoder.hh
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <memory>
+#include <x264.h>
+
+#include "raster.hh"
+#include "spans.hh"
+
+class H264Encoder
+{
+private:
+  struct x264_deleter
+  {
+    void operator()( x264_t* x ) const { x264_encoder_close( x ); }
+  };
+
+  std::unique_ptr<x264_t, x264_deleter> encoder_ {};
+  x264_param_t params_ {};
+  x264_picture_t pic_in_ {};
+  int frame_size_ {};
+
+  uint16_t width_;
+  uint16_t height_;
+  uint8_t fps_;
+  uint32_t frame_num_ {};
+
+public:
+  H264Encoder( const uint16_t width,
+               const uint16_t height,
+               const uint8_t fps,
+               const std::string& preset,
+               const std::string& tune );
+
+  std::string_view encode( RasterYUV420& raster );
+};
