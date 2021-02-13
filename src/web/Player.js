9f1d90d3f5d64aa6d9ff69d6768b18385329815b
Keith Winstein <keithw@cs.stanford.edu>
2021-02-13 10:44:59 -0800

    JS for WebSocket?

--- src/web/Player.js
+++ src/web/Player.js
@@ -14,11 +14,14 @@ window.onload = async function(e){
     }
 }
 
-async function sourceOpen(e) {
+function sourceOpen(e) {
     const audio = document.getElementById('audio');
     var mime = "audio/webm; codecs=\"opus\"";
     var mediaSource = this;
     var firstplay = true;
+    var sourceBuffer = mediaSource.addSourceBuffer(mime);
+    var firstmessage = true;
+
     queue.push = function( buffer ) { 
         // in case the message comes late
         if ( !sourceBuffer.updating && this.length == 0) { 
@@ -28,10 +31,26 @@ async function sourceOpen(e) {
         } 
     }
 
-    var firstmessage = true;
-    var sourceBuffer = mediaSource.addSourceBuffer(mime);
-    const response = await fetch('http://127.0.0.1:8080');
-    const reader = response.body.getReader();
+    ws = new WebSocket("wss://stagecast.org:8081")
+    ws.onmessage = function( e ) {
+        if(sourceBuffer.buffered.length > 0){
+            if(firstplay && audio.played.length == 0){ // if you start after the start
+                firstplay = false;
+                audio.currentTime = sourceBuffer.buffered.start(0);
+            }
+            console.log("buffered", sourceBuffer.buffered.start(0), sourceBuffer.buffered.end(0));
+        }
+        if(audio.played.length > 0){
+            console.log("played", audio.played.start(0), audio.played.end(0));
+        }
+        if(!firstmessage){
+            queue.push(e);
+        } else {
+            firstmessage = false;
+            sourceBuffer.appendBuffer(e);
+        }
+    }
+
     sourceBuffer.addEventListener('abort', function(e) {
         console.log('audio source buffer abort:', e);
     });
@@ -45,26 +64,4 @@ async function sourceOpen(e) {
             sourceBuffer.appendBuffer(data);
         }
       });
-    while(true){
-        const {value, done} = await reader.read();
-        if(done){
-            break;
-        }
-        if(sourceBuffer.buffered.length > 0){
-            if(firstplay && audio.played.length == 0){ // if you start after the start
-                firstplay = false;
-                audio.currentTime = sourceBuffer.buffered.start(0);
-            }
-            console.log("buffered", sourceBuffer.buffered.start(0), sourceBuffer.buffered.end(0));
-        }
-        if(audio.played.length > 0){
-            console.log("played", audio.played.start(0), audio.played.end(0));
-        }
-        if(!firstmessage){
-            queue.push(value);
-        } else {
-            firstmessage = false;
-            sourceBuffer.appendBuffer(value);
-        }   
-    }
 }
