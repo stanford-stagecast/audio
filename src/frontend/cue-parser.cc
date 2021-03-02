36eeafea8d7ca9b2a5a39df2f2827746134eaf8a
Keith Winstein <keithw@cs.stanford.edu>
2021-03-01 16:35:19 -0800

    Parse camera and media events

--- src/frontend/cue-parser.cc
+++ src/frontend/cue-parser.cc
@@ -12,6 +12,19 @@
 
 using namespace std;
 
+struct View
+{
+  uint32_t top, right, bottom, left, z;
+
+  View( const Json::Value& node )
+    : top( node["top"].asUInt() )
+    , right( node["right"].asUInt() )
+    , bottom( node["bottom"].asUInt() )
+    , left( node["left"].asUInt() )
+    , z( node["z"].asUInt() )
+  {}
+};
+
 int main( int argc, char* argv[] )
 {
   if ( argc <= 0 ) {
@@ -32,7 +45,7 @@ int main( int argc, char* argv[] )
   istr >> root;
 
   /* learn camera mapping */
-  unordered_map<int, string> camera_id_to_name;
+  unordered_map<int, string> camera_lookup;
   for ( auto it = root["cameras"].begin(); it != root["cameras"].end(); it++ ) {
     const auto node = *it;
     const int id = node["id"].asInt();
@@ -47,14 +60,16 @@ int main( int argc, char* argv[] )
       name = "Josh";
     }
 
-    camera_id_to_name.insert_or_assign( id, name );
+    camera_lookup.insert_or_assign( id, name );
   }
 
-  /* list media */
+  /* learn media */
+  unordered_map<int, pair<string, string>> media_lookup;
   for ( auto it = root["media"].begin(); it != root["media"].end(); it++ ) {
     const auto node = *it;
-    //    const int id = node["id"].asInt();
+    const int id = node["id"].asInt();
     const string url = node["file"].asString();
+    const string name = node["name"].asString();
     const string type = node["type"].asString();
 
     string filename = url;
@@ -66,7 +81,48 @@ int main( int argc, char* argv[] )
       }
     }
 
-    cout << "wget -O " << filename << " " << url << "\n";
+    if ( type == "image" ) {
+      filename += ".mp4";
+    }
+
+    media_lookup.insert_or_assign( id, make_pair( name, filename ) );
+  }
+
+  /* go through cues in order */
+  for ( auto it = root["cues"].begin(); it != root["cues"].end(); it++ ) {
+    const auto cue = *it;
+    const string cue_number_raw = cue["number"].asString();
+    if ( cue_number_raw.size() < 2 ) {
+      throw runtime_error( "invalid cue number: " + cue_number_raw );
+    }
+    const unsigned int cue_integer = stoi( cue_number_raw.substr( 0, cue_number_raw.size() - 2 ) );
+    const unsigned int cue_fractional = stoi( cue_number_raw.substr( cue_number_raw.size() - 3, 1 ) );
+    cout << cue_integer << "." << cue_fractional << ": " << cue["name"] << "\n";
+
+    const auto changes = cue["changes"];
+
+    for ( auto it2 = changes.begin(); it2 != changes.end(); ++it2 ) {
+      const auto change = *it2;
+
+      const auto action_type = change["action"]["type"];
+      const auto change_type = change["type"];
+
+      if ( action_type == "add" and change_type == "camera" ) {
+        cout << "   ADD camera " << camera_lookup.at( change["camera_id"].asInt() ) << "\n";
+
+        View v { change["action"]["config"]["view"] };
+        cout << "        @ " << v.left << " " << v.right << " " << v.top << " " << v.bottom << " " << v.z << "\n";
+      } else if ( action_type == "add" and change_type == "media" ) {
+        cout << "   ADD media \"" << media_lookup.at( change["media_id"].asInt() ).first << "\"\n";
+
+        View v { change["action"]["config"]["view"] };
+        cout << "        @ " << v.left << " " << v.right << " " << v.top << " " << v.bottom << " " << v.z << "\n";
+      } else if ( action_type == "remove" and change_type == "camera" ) {
+        cout << "   REMOVE camera " << camera_lookup.at( change["camera_id"].asInt() ) << "\n";
+      } else if ( action_type == "remove" and change_type == "media" ) {
+        cout << "   REMOVE media \"" << media_lookup.at( change["media_id"].asInt() ).first << "\"\n";
+      }
+    }
   }
 
   return EXIT_SUCCESS;
